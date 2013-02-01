eca8e0c241531803256fc11d2ceb3c68ef3caa15
lnafziger <lnafziger@gmail.com>
2013-02-01 15:37:26 -0500

    v0.9.2 - re-licensed Numberpad
    All files - Changed to MIT license

--- Example/ViewController.m
+++ Example/ViewController.m
@@ -8,20 +8,18 @@
  * Copyright 2012 Louis Nafziger
  ******************************************************************************
  *
- * This file is part of NumberPad.
+ * COPYRIGHT 2013 Louis Nafziger
  *
  * NumberPad is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the The MIT License (MIT).
  *
  * NumberPad is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * The MIT License for more details.
  *
- * You should have received a copy of the GNU LesserGeneral Public License
- * along with NumberPad.  If not, see <http://www.gnu.org/licenses/>. *
+ * You should have received a copy of the The MIT License (MIT)
+ * along with NumberPad.  If not, see <http://opensource.org/licenses/MIT>.
  *
  *****************************************************************************/
 
